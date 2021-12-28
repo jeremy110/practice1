61c726f0fc6ec72ef59ced35ab20cd523628012a
jeremy110 <52516810+jeremy110@users.noreply.github.com>
2021-12-28 15:52:08 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 const {findOne, insert, deleteOne,create_table,client_end} = require('./route/controller');
-
+const router = require('./route/controller_new');
+// var {router} = require('./route/controller');
 app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 require('dotenv').config();
 
 //HTTP當中有GET POST PUT DELETE
-app.get('/select/:id',findOne);
-app.post('/insert/:id',insert);
-app.put('/update-data',(req,res)=>{ 
-    res.send('PUT Resquest');
-});
-app.delete('/delete/:id',deleteOne);
-app.get('/',create_table);
-app.get('/end',client_end);
+app.use('/api/v1',router);
+//app.use('/select/:id',router);
+// app.use(select);
+//app.get('/select/:id',findOne);
+// app.post('/insert/:id',insert);
+// app.put('/update-data',(req,res)=>{ 
+//     res.send('PUT Resquest');
+// });
+// app.delete('/delete/:id',deleteOne);
+// app.get('/',create_table);
+// app.get('/end',client_end);
 
 var server = app.listen(5050,function(){
     console.log('Node server is running');
--- route/controller_new.js
+++ route/controller_new.js
@@ -10,44 +10,28 @@ client.connect(err => {
     }
 });
 
-// router.get('/',(req, res, next) => {
-//     const id = req.params.id;
-//     const text = `SELECT * FROM ${id}`;
-//     client.query(text,(_err, _res) => {
-//         if (_err) {
-//             console.error(_err);
-//             res.status(400).send("select error");
-//             client.end();
-//             return;
-//         }
-//         for (let row of _res.rows) {
-//             console.log(row);
-//         }
-//         res.status(200).send(_res.rows);
-//         //client.end();
-//     });
-// })
 
-// module.exports = router;
-// exports.findOne = (req, res) => {
-//     //client.connect();
-//     const id = req.params.id;
-//     const text = `SELECT * FROM ${id}`;
-//     client.query(text,(_err, _res) => {
-//         if (_err) {
-//             console.error(_err);
-//             res.status(400).send("select error");
-//             client.end();
-//             return;
-//         }
-//         for (let row of _res.rows) {
-//             console.log(row);
-//         }
-//         res.status(200).send(_res.rows);
-//         //client.end();
-//     });
-// }
-exports.insert = (req, res) => {
+//select data
+router.get('/select/:id', (req, res) => {
+    const id = req.params.id;
+    const text = `SELECT * FROM ${id}`;
+    client.query(text,(_err, _res) => {
+        if (_err) {
+            console.error(_err);
+            res.status(400).send("select error");
+            client.end();
+            return;
+        }
+        for (let row of _res.rows) {
+            console.log(row);
+        }
+        res.status(200).send(_res.rows);
+        //client.end();
+    });
+})
+
+//create new data
+router.post('/insert/:id', (req, res) => {
     const id = req.params.id;
     var {email,firstname,lastname,age} = req.body;
     const text = 
@@ -65,9 +49,10 @@ exports.insert = (req, res) => {
             res.status(200).send("insert finish");
             //client.end();
         });
-}
+})
 
-exports.deleteOne = (req, res) => {
+//delete data
+router.delete('/delete/:id', (req, res) => {
     const id = req.params.id;
     const text =`DELETE FROM ${id}`;
 
@@ -81,9 +66,10 @@ exports.deleteOne = (req, res) => {
         res.status(200).send("delete finish");
         //client.end();
     });
-}
+})
 
-exports.create_table = (req, res) => {
+//create table
+router.get('/', (req, res) => {
     const text = `
     CREATE TABLE u(
     email       varchar,
@@ -101,9 +87,6 @@ exports.create_table = (req, res) => {
         res.status(200).send("create table u");
         //client.end();
     });
-}
+})
 
-exports.client_end = (req, res) => {
-    client.end();
-    res.status(200).send("disconnected");
-}
+module.exports = router;
